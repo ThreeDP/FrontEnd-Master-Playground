cbd1e4fbe75cd88e069b7f27e1dc53fd50620481
Threedp <davypaulinocbjr@outlook.com>
2021-07-26 21:04:02 -0300

    implementação do botões de operadores

--- my-scripts.js
+++ my-scripts.js
@@ -3,24 +3,24 @@ const visionCalc = document.querySelector('.vision-calc');
 const visionResult = document.querySelector('.vision-result');
 
 let calculate = {
-    caracterAction: '/',
-    firstNum: 30,
-    secondNum: 2,
+    caracterAction: " ",
+    firstNum: 0,
+    secondNum: 0,
 }
 
 function calcSub(n1, n2) {
-    return n1 + n2;
+    return n1 - n2;
 }
 
 function calcSum(n1, n2) {
-    return n1 - n2;
+    return n1 + n2;
 }
 
 function calcMul(n1, n2) {
     return n1 * n2;
 }
 
-function calcSub(n1, n2) {
+function calcDiv(n1, n2) {
     return n1 / n2;
 }
 
@@ -34,7 +34,7 @@ function resultCalc () {
     } else if (calculate.caracterAction == "*") {
         result = calcMul(calculate.firstNum, calculate.secondNum);
     } else if (calculate.caracterAction == "/") {
-        result = calcSub(calculate.firstNum, calculate.secondNum);
+        result = calcDiv(calculate.firstNum, calculate.secondNum);
     } else {
         return;
     }
@@ -42,6 +42,26 @@ function resultCalc () {
     visionCalc.innerText = 0;
 };
 
+// Inserção dos numeros na Vision
+function enterNum (num) {
+    if (visionCalc.innerText.substring(0, 1) === '0') {
+        visionCalc.innerText = num;
+    } else {
+        visionCalc.innerText += num;
+    }
+}
+
+function operationAction (operator) {
+    if (visionResult.innerText.substring(0, 1) === "0" ) {
+        calculate.caracterAction = operator;
+        calculate.firstNum = parseInt(visionCalc.innerText)
+        visionResult.innerText = visionCalc.innerText + " " + operator + " ";
+    } else {
+        calculate.secondNum = parseInt(visionCalc.innerText)
+        resultCalc();
+    }
+    visionCalc.innerText = 0;
+}
 
 // defiine qual o tipo de button de entrada.
 document.querySelector('.calc').addEventListener('click', function(event) {
@@ -49,16 +69,22 @@ document.querySelector('.calc').addEventListener('click', function(event) {
         // Pega a ultima class da propriedade Class
         let classNameButton = event.target.className;
         let index = event.target.className.lastIndexOf(" ")+1;
+
         if(classNameButton.substring(index) === 'number-button') {
-            alert("number")
+            enterNum(event.target.innerText);
+
         } else if (classNameButton.substring(index) === 'operation-button'){
-            alert("operação")
+            operationAction(event.target.innerText);
+
         } else if (classNameButton.substring(index) === 'result-button') {
             resultCalc();
+
         } else if (classNameButton.substring(index) === 'correct-button') {
             alert("correção")
+
         } else if (classNameButton.substring(index) === 'del-button') {
             alert("delete")
+
         }
     }
     
